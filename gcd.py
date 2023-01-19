4c97ae205a7f7527a23e619dc89b9a50ad9bd9e3
ChandanaPenumuchu31 <121151964+ChandanaPenumuchu31@users.noreply.github.com>
2023-01-19 10:42:44 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,17 +1,16 @@
 import random
-try:
-    num1=random.randint(1,10000)
-    try:
-      num2=random.randint(1,100000)
-      gcd=min(num1,num2)
-      print("Number 1: ",num1)
-      print("Number 2 : ",num2)
-      while(gcd>=1):
-        if(int(num1%gcd)==0 and int(num2%gcd)==0):
+num1=random.randint(1,100000)
+num2=random.randint(1,100000)
+if(isinstance(num1,int) and isinstance(num2,int)):
+    gcd=min(num1,num2)
+    print("Number 1: ",num1)
+    print("Number 2 : ",num2)
+    while(gcd>=1):
+       if(int(num1%gcd)==0 and int(num2%gcd)==0):
            break
-        gcd=gcd-1
-      print("GCD of two numbers is : ",gcd)
-    except:
-      print("num2 is not an integer")
-except:
-    print("num1 i not an integer")
+       gcd=gcd-1
+    print("GCD of two numbers is : ",gcd)
+else:
+    print("Type of numbers ")
+    print("num1 : ",type(num1))
+    print("num2 : ",type(num2))
